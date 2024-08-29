f2bb4e4b82bfe3f1698e6c55b05b7d9896ac02ee
AndreyMonti <andreymontibeller02@gmail.com>
2024-08-29 10:43:26 -0300

    correcao-streaming

--- canais_streaming/streaming.js
+++ canais_streaming/streaming.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    function showItem(index) {
+        items.forEach((item, i) => {
+            item.classList.remove('active');
+            if (i === index) {
+                item.classList.add('active');
+            }
+        });
+    }
+
+    function nextItem() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        showItem(currentIndex);
+    }
+
+    function prevItem() {
+        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+        showItem(currentIndex);
+    }
+
+    document.querySelector('.carousel-controls .next').addEventListener('click', nextItem);
+    document.querySelector('.carousel-controls .prev').addEventListener('click', prevItem);
+
+    showItem(currentIndex); // Mostrar o primeiro item inicialmente
+});
+
+
 document.addEventListener('scroll', function() {
     const callToAction = document.querySelector('.call-to-action');
     const scrollPosition = window.scrollY + window.innerHeight;
