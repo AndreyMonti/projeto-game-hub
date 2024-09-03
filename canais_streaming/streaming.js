a1e7a86706561a0566c8c81d1d706b63191ddb49
AndreyMonti <andreymontibeller02@gmail.com>
2024-09-03 20:16:31 -0300

    sidebar-1ver

--- canais_streaming/streaming.js
+++ canais_streaming/streaming.js
@@ -42,3 +42,33 @@ document.addEventListener('scroll', function() {
         callToAction.classList.remove('visible');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var header = document.getElementById('header');
+    var navigationHeader = document.getElementById('navigation_header');
+    var content = document.querySelector('.hero'); // Atualizado para selecionar a seção hero
+    var menuToggle = document.getElementById('menu-toggle');
+    var sidebar = document.getElementById('sidebar');
+    var closeSidebar = document.getElementById('close-sidebar');
+    var showSidebar = false;
+
+    function toggleSidebar() {
+        showSidebar = !showSidebar;
+        if (showSidebar) {
+            sidebar.style.left = '0';
+            content.classList.add('blur');
+        } else {
+            sidebar.style.left = '-100vw';
+            content.classList.remove('blur');
+        }
+    }
+
+    menuToggle.addEventListener('click', toggleSidebar);
+    closeSidebar.addEventListener('click', toggleSidebar);
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768 && showSidebar) {
+            toggleSidebar(); // Fecha a sidebar quando a tela é redimensionada para um tamanho maior
+        }
+    });
+});
--- tela-inicial/tela-inicial.js
+++ tela-inicial/tela-inicial.js
@@ -1,34 +1,3 @@
-let slideIndex = 0;
-showSlides(slideIndex);
-
-function moveSlide(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n - 1);
-}
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("carousel-item");
-    const dots = document.getElementsByClassName("dot");
-
-    if (n >= slides.length) { slideIndex = 0; }
-    if (n < 0) { slideIndex = slides.length - 1; }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex].style.display = "block";
-    dots[slideIndex].className += " active";
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var header = document.getElementById('header');
     var navigationHeader = document.getElementById('navigation_header');
