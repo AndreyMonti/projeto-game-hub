9e8d06664238058ff0d6e3b3ce0dbc3aeaec7eff
AndreyMonti <andreymontibeller02@gmail.com>
2024-11-14 11:17:29 -0300

    suaconta-finalizacao

--- sua-conta/sua-conta.php
+++ sua-conta/sua-conta.php
@@ -15,27 +15,30 @@ $usuario_tipo = $_SESSION['tipo_sessao'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alterar Informações</title>
+    <title>Alteração de Dados</title>
     <link rel="stylesheet" href="sua-conta.css">
 </head>
 <body>
-    <div class="back-button">
-        <a href="./tela-inicial/tela-inicial.php">&#8592;</a>
+    <div class="back-arrow">
+        <a href="../tela-inicial/tela-inicial.php">&#8592;</a>
     </div>
-    <div class="form-container">
-        <div class="profile-icon"></div>
-        <form action="processa_alteracao.php" method="POST">
-            <label for="nome">Alterar nome:</label>
-            <input type="text" id="nome" name="nome" required>
-
-            <label for="email">Alterar E-mail:</label>
-            <input type="email" id="email" name="email" required>
-
-            <label for="senha">Alterar senha:</label>
-            <input type="password" id="senha" name="senha" required>
-
-            <button type="submit" class="confirm-button">Confirmar</button>
-        </form>
+    <div class="container">
+        <div class="top-bar"></div>
+        <div class="form-container">
+            <div class="avatar"></div>
+            <form action="#" method="POST">
+                <label for="name">Alterar nome:</label>
+                <input type="text" id="name" name="name" placeholder="Digite seu nome">
+                
+                <label for="email">Alterar E-mail:</label>
+                <input type="email" id="email" name="email" placeholder="Digite seu email">
+                
+                <label for="password">Alterar senha:</label>
+                <input type="password" id="password" name="password" placeholder="Digite sua senha">
+                
+                <button type="submit">Confirmar</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
