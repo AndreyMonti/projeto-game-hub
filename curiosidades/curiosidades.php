2fdc3cd0d9e968584da3cee7a7fbb6226b2e8c3d
JuliaConconi <conconijulia@gmail.com>
2024-11-21 14:24:58 -0300

    curiosidades

---
+++ curiosidades/curiosidades.php
@@ -0,0 +1,47 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="curiosidades.css">
+</head>
+<body>
+    <header>
+        <a href="index.php">
+            <img src="imagens/Icone_Controle.png" alt="Ícone" class="logo">
+        </a>
+        <div class="search-bar">
+            <input type="text" placeholder="O que deseja descobrir?">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Curiosidades</a></li>
+                <li><a href="#">Eventos</a></li>
+                <li><a href="#">Transmissões</a></li>
+                <li><a href="#">Sua conta</a></li>
+            </ul>
+        </nav>
+    </header>   
+    
+        <div class="video-container">
+            <img src="curiosidades.jpg" alt="img_curiosidades">
+            <div class="overlay"></div>
+            <div class="text-overlay">
+                <h1>Veja mais: descubra 10 curiosidades e dados da indústria dos jogos</h1>
+            </div>
+        </div>
+        <div class="text-content">
+            <ul>
+                <li><strong> Fall Guys, desenvolvido por um estúdio indie e por uma publisher especialista em jogos independentes, atinge sete milhões de vendas em menos de um mês.</strong> Criado pela Mediatonic, que fez jogos web desde 2005, esse título teve propaganda da publicadora Devolver e foi estouro de público com party games com feijões coloridos e customizáveis. Foram milhões de cópias no Steam e o game mais baixado da PSN, porque ele entrou de graça na PS Plus de agosto de 2020.</li>
+                <li><br><strong>Nintendo é a segunda empresa com mais consoles vendidos entre os 10 primeiros, somando 455,52 milhões de unidades.</strong>O número soma as vendas, na ordem, da família Nintendo DS (154,02 milhões), da família Game Boy (118,69 milhões), do Wii (101,3 milhões) e da família Game Boy Advance (81,51 milhões). Os portáteis sempre foram importantes na trajetória da Big N.</li>
+                <li><br><strong>No começo da pandemia de coronavírus, a indústria cresceu US$ 10 bilhões: o dado é da Superdata, da Nielsen.</strong></li>
+                <li><br><strong>O celular é a plataforma mais utilizada pelo brasileiro para jogar, com 86,7% da preferência, de acordo com a PGB.</strong> Num país com 220 milhões de smartphones, segundo a FGV (Fundação Getúlio Vargas).</li>
+            </ul>
+        </div>
+</body>
+</html>
